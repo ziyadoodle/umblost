79eeb4f8641a776bde8b34111199e6c405ea1c36
ziyadoodle <ziyadzidane69@gmail.com>
2025-08-11 13:09:56 +0700

    google signin (OAuth2, Lucia)

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 import loginImage from "@/assets/login-image.jpg";
+import GoogleSignInButton from "./GoogleSignInButton";
 
 export const metadata: Metadata = {
     title: "Login",
@@ -15,6 +16,12 @@ export default function Page() {
             <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
                 <h1 className="text-center text-3xl font-bold">Login to UMBLost</h1>
                 <div className="space-y-5">
+                    <GoogleSignInButton />
+                    <div className="flex items-center gap-3">
+                        <div className="h-px flex-1 bg-muted"></div>
+                        <span className="text-xs text-muted-foreground">OR</span>
+                        <div className="h-px flex-1 bg-muted"></div>
+                    </div>
                     <LoginForm />
                     <Link href="/signup" className="block text-center hover:underline">
                         Don&apos;t have an account? Sign up
--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -34,13 +34,13 @@ export default function UserButton({ className }: UserButtonProps) {
             <DropdownMenuSeparator />
             <Link href={`/users/${user.username}`}>
                 <DropdownMenuItem>
-                    <UserIcon className="mr-2 size-4" />
+                    <UserIcon className="mr-2 size-4 text-muted-foreground" />
                     Profile
                 </DropdownMenuItem>
             </Link>
             <DropdownMenuSub>
                 <DropdownMenuSubTrigger>
-                    <Monitor className="mr-2 size-4" />
+                    <Monitor className="mr-2 size-4 text-muted-foreground" />
                     Theme
                 </DropdownMenuSubTrigger>
                 <DropdownMenuPortal>
@@ -68,7 +68,7 @@ export default function UserButton({ className }: UserButtonProps) {
                 queryClient.clear();
                 logout()
             }}>
-                <LogOutIcon className="mr-2 size-4" />
+                <LogOutIcon className="mr-2 size-4 text-muted-foreground" />
                 Logout
             </DropdownMenuItem>
         </DropdownMenuContent>
