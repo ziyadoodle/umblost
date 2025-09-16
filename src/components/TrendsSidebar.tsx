b54fbb98091682be973339a273dc73231fbc7044
ziyadoodle <ziyadzidane69@gmail.com>
2025-09-16 10:37:18 +0700

    clear unused vars

--- src/components/TrendsSidebar.tsx
+++ src/components/TrendsSidebar.tsx
@@ -1,14 +1,9 @@
-import { validateRequest } from "@/auth"
 import prisma from "@/lib/prisma";
 import { Loader2 } from "lucide-react";
 import Link from "next/link";
 import { Suspense } from "react";
-import UserAvatar from "./UserAvatar";
 import { unstable_cache } from "next/cache";
 import { formatNumber } from "@/lib/utils";
-import FollowButton from "./FollowButton";
-import { getUserDataSelect } from "@/lib/types";
-import UserTooltip from "./UserTooltip";
 import GuideButton from "./GuideButton";
 
 export default function TrendsSidebar() {
@@ -28,60 +23,6 @@ function Guide() {
         <>
             <GuideButton />
         </>
-    )
-}
-
-async function WhoToFollow() {
-    const { user } = await validateRequest();
-
-    if (!user) return null;
-
-    const usersToFollow = await prisma.user.findMany({
-        where: {
-            NOT: {
-                id: user.id
-            },
-            followers: {
-                none: {
-                    followerId: user.id
-                }
-            }
-        },
-        select: getUserDataSelect(user.id),
-        take: 5
-    })
-
-    return (
-        <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
-            <div className="text-xl font-bold">Who to follow</div>
-            {usersToFollow.map(user => (
-                <div key={user.id} className="flex items-center justify-between gap-3">
-                    <UserTooltip user={user}>
-                        <Link
-                            href={`/users/${user.nim}`}
-                            className="flex items-center gap-3">
-                            <UserAvatar avatarUrl={user.avatarUrl} className="flex-none" />
-                            <div>
-                                <p className="line-clamp-1 break-all font-semibold hover:underline">
-                                    {user.name}
-                                </p>
-                                <p className="line-clamp-1 break-all text-muted-foreground">
-                                    @{user.nim}
-                                </p>
-                            </div>
-                        </Link>
-                    </UserTooltip>
-                    <FollowButton
-                        userId={user.id}
-                        initialState={{
-                            followers: user._count.followers,
-                            isFollowedByUser: user.followers.some(
-                                ({ followerId }) => followerId === user.id
-                            )
-                        }} />
-                </div>
-            ))}
-        </div>
     )
 }
 
